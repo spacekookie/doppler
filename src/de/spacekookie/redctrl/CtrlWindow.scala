c8157551949a71f6b5f7d4d946b1df6f59709a84
Katharina Sabel <sabel.katharina@gmail.com>
2017-02-06 10:09:24 +0100

    Cleaning code and changing UI  - Sliders now have an entry box  - HBox layouts in VBox  - Using Option to post-init values

--- src/de/spacekookie/redctrl/CtrlWindow.scala
+++ src/de/spacekookie/redctrl/CtrlWindow.scala
@@ -3,35 +3,58 @@ package de.spacekookie.redctrl
 import org.gnome.gdk.Event
 import org.gnome.gtk._
 
+/** Define our data case class up top */
+case class SliderBar(slider: Scale, box: Entry, layout: HBox)
+
+
+/** Main UI controller object */
 object Controller {
-  val DEF_UPPER : Int = 5500
-  val DEF_LOWER : Int = 2000
+  val DEF_UPPER: Int = 5500
+  val DEF_LOWER: Int = 2000
+
+  var nSlider: Option[SliderBar] = null
+  var dSlider: Option[SliderBar] = null
+
   
+  /** Main function that starts the application & UI logic */
   def main(args: Array[String]) {
-    Gtk.init(args);
 
-    val window = new Window();
-    val save = new Button(" Save ");
-    val quit = new Button(" Quit ");
-    val transition = new CheckButton("Enable day-night transition");
-    val layout = new VBox(false, 3);
+    Gtk.init(args)
 
-    window.setTitle("Redshift Control");
-    window.add(layout);
-    window.setSizeRequest(350, 250);
+    val window = new Window()
+    val save = new Button(" Save ")
+    val quit = new Button(" Quit ")
+    val transition = new CheckButton("Enable day-night transition")
+    val layout = new VBox(false, 3)
 
-    layout.add(new Label(" Setup Redshift boundry values "));
-    layout.add(new HSeparator);
+    /** Setup the main window */
+    window.setTitle("Redshift Control")
+    window.add(layout)
+    window.setSizeRequest(350, 250)
 
-    layout.add(sliderFactory("Day Temperature", 4500));
-    layout.add(sliderFactory("Nights Temperature", 3250));
+    /** Top label and a separator */
+    layout.add(new Label(" Setup Redshift boundry values "))
+    layout.add(new HSeparator)
 
-    layout.add(transition);
-    layout.add(new HSeparator);
+    /** Include two sliders */
+    dSlider = Option(makeSlider("Day Temperature", 4500))
+    layout.add(dSlider.get.layout)
     
-    layout.add(save);
-    layout.add(quit)
-    window.showAll();
+    nSlider = Option(makeSlider("Night Temperature", 3250))
+    layout.add(nSlider.get.layout)
+    
+    /** Include some additional options */
+    layout.add(transition)
+    layout.add(new HSeparator)
+
+    /** Save and quit buttons in an HBox */
+    val bBox = new HBox(false, 2)
+    bBox.add(save)
+    bBox.add(quit)
+    layout.add(bBox)
+    
+    /** Show all window elements */
+    window.showAll()
 
     window.connect(new Window.DeleteEvent {
       def onDeleteEvent(w: Widget, e: Event) = {
@@ -40,17 +63,31 @@ object Controller {
       }
     })
 
+    /** Start Gtk main loop */
     Gtk.main();
   }
   
-  private def sliderFactory(name: String, default: Int) : Scale = {
-    var adj = new Adjustment();
-    adj.setLower(DEF_LOWER);
-    adj.setUpper(DEF_UPPER);
-    adj.setValue(default);
+  
+  /** Create slider bars with nicely initialised values */
+  private def makeSlider(name: String, default: Int): SliderBar = {
+    val adj = new Adjustment()
+    adj.setLower(DEF_LOWER)
+    adj.setUpper(DEF_UPPER)
+    adj.setValue(default)
     
-    var scale = new Scale(Orientation.HORIZONTAL, adj);
-    scale.setTooltipText(name);
-    return scale;
+    /** Create the slider-scale */
+    val scale = new Scale(Orientation.HORIZONTAL, adj)
+    scale.setTooltipText(name)
+    scale.setDrawValue(false)
+    
+    val box = new Entry
+    val layout = new HBox(true, 2)
+    
+    layout.add(box)
+    layout.add(scale)
+    
+    /** Return a new instance of sliderbar */
+    return new SliderBar(scale, box, layout)
   }
+
 }
